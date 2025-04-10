0b707bc103704fcc35ab229703991ecbf44f375a
sainatha-reddy <sainathreddy057@gmail.com>
2025-04-10 11:47:42 +0530

    Updated HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div>
         <video width="100%" height="auto" loop muted autoPlay>
-            <source src="https://videos.pexels.com/video-files/2268807/2268807-hd_1920_1080_24fps.mp4" type="video/mp4" />
+            <source src="https://videos.pexels.com/video-files/4605192/4605192-uhd_4096_2160_25fps.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
     </div>
